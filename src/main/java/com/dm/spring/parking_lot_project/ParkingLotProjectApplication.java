374e16b011602ed6c9fc54c8e0b429ae5dc205aa
dwaipayan-dev <dwaipayan.vidyanta1998@gmail.com>
2022-09-05 19:02:42 +0530

    first commit

---
+++ src/main/java/com/dm/spring/parking_lot_project/ParkingLotProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dm.spring.parking_lot_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingLotProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingLotProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dm/spring/parking_lot_project/ParkingLotProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dm.spring.parking_lot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingLotProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
