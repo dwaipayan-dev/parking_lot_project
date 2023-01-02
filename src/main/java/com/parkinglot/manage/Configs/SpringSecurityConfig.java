cdfb8882ce151a277ae94162ed82fe8963bb93b6
dwaipayan-dev <dwaipayan.vidyanta1998@gmail.com>
2023-01-02 20:06:51 +0530

    jwtauthfeature: Authentication is complete

--- src/main/java/com/parkinglot/manage/Configs/SpringSecurityConfig.java
+++ src/main/java/com/parkinglot/manage/Configs/SpringSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.parkinglot.manage.Configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.parkinglot.manage.Filter.JwtFilter;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -14,6 +19,9 @@ public class SpringSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Autowired
+    private JwtFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,11 +33,16 @@ public class SpringSecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/admin/signup", "/admin/").permitAll()
+                .antMatchers("/admin/signup", "/admin/login").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .httpBasic();
+
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
